a93793c89826788c1e5ecaf872e4ac2e29082f59
Febry Prasetya <71777049+fbpr@users.noreply.github.com>
2023-06-20 23:46:12 +0700

    Time: 68 ms (22.73%), Space: 42 MB (39.84%) - LeetHub

---
+++ 2749-promise-time-limit/2749-promise-time-limit.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+	return async function(...args) {
+        return new Promise(async (resolve, reject) => {
+            setTimeout(() => reject("Time Limit Exceeded"), t);
+            fn(...args).then(resolve, reject);
+            clearTimeout(this);
+        });
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
